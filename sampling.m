b98f160921fcd5f26b34e3c563bd4a9ff4def807
Ahmet Cihat Bozkurt <ahmetcihat2773@gmail.com>
2021-12-01 21:45:05 +0100

    Update sampling.m

--- sampling.m
+++ sampling.m
@@ -13,7 +13,7 @@ function [trainData,validData] = sampling(data, percentage, type)
 
     % returns :: train and validation data with the same structure as data
     % has.
-    
+
     samples = data.x;
     labels = data.y;
     data_size = length(data.y)
@@ -39,8 +39,8 @@ function [trainData,validData] = sampling(data, percentage, type)
         % 3- Find the necessary points that should be chosen according to the
         % trainvalid split rate and class rate. 
 
-         num_zero = sum(data.y == 0);
-         num_one = sum(data.y == 1);
+         num_zero = sum(data.y == labels(1));
+         num_one = sum(data.y == labels(2));
          % Total number of sample points for each class.
          num_zero_percentage = num_zero/data_size;
          num_one_percentage = num_one/data_size;
@@ -48,27 +48,37 @@ function [trainData,validData] = sampling(data, percentage, type)
          select_one_training = round(num_one_percentage*percentage*data_size);
          % 105
          select_zero_training = round(num_zero_percentage*percentage*data_size);
-         tot_num_training = select_zero_training + select_one_training ;
-         % 70 etc
+         
+         one_samples = data.x(data.y==labels(2),:);
+         one_samples_train = one_samples(1:select_one_training,:);
+         zero_samples = data.x(data.y==labels(1),:);
+         zero_samples_train = zero_samples(1:select_zero_training,:);
 
 
-         one_samples_train = data.x(data.y==1,:);
-         zero_samples_train = data.x(data.y==0,:);
-         one_label_train = data.y(data.y == 1);
-         zero_label_train = data.y(data.y == 0);
+         one_label = data.y(data.y == labels(2));
+         zero_label = data.y(data.y == labels(1));
+         zero_label_train = zero_label(1:select_zero_training);
+         one_label_train = one_label(1:select_one_training,:);
 
-         % Label 0 and Label 1 INDEXES
+        trainData.x = [one_samples_train;zero_samples_train];
+        trainData.y = [one_label_train;zero_label_train];
 
-         final_index = randperm(tot_num_training)';
-
-        trainData.x = [zero_samples_train(1:select_zero_training,:) ; one_samples_train(1:select_one_training,:)];
+        tot_num_training = length(trainData.y);
+        final_index = randperm(tot_num_training)';
         trainData.x = trainData.x(final_index,:);
-        trainData.y = [one_label_train(1:select_one_training,1) ;zero_label_train(1:select_zero_training,1) ];
-        trainData.y = trainData.y(final_index,1);
+        trainData.y = trainData.y(final_index,:);
 
         % Take first n sample for x values which are labeled as 1.      
-        validData.x = [zero_samples_train(select_zero_training+1:end,:) ; one_samples_train(select_one_training+1:end,:)]
-        validData.y = [one_label_train((select_one_training+1):end,1) ;zero_label_train((select_zero_training+1):end,1) ];
+        one_samples_valid = one_samples(select_one_training+1:end,:);
+        zero_samples_valid = zero_samples(select_zero_training+1:end,:);
+        
+        zero_label_valid = zero_label(select_zero_training+1:end);
+        one_label_valid = one_label(1+select_one_training:end);
+
+      
+       
+        validData.x = [one_samples_valid;zero_samples_valid];        
+        validData.y = [one_label_valid ;zero_label_valid ];
         validData_len = randperm(length(validData.y))';
         % Mix the data points randomly. 
         validData.x = validData.x(validData_len,:);
